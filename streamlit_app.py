b650261a95d9dfa0452e13d47c2441b67f65939c
rmareedu2293 <31863929+rmareedu2293@users.noreply.github.com>
2022-06-10 20:03:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
-streamlit.title("May name is Ramesh")
+streamlit.title("My parents new healthy Diner")
+streamlit.header('Breakfast Menu')
+streamlit.txt('kale,Spinach & Rocket Smoothie')
+streamlit.txt('Hard Boiled Free Range Egg')
