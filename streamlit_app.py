880315db865d539bc9a693d341d3562a2c48bb82
rmareedu2293 <31863929+rmareedu2293@users.noreply.github.com>
2022-06-10 20:06:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.txt('kale,Spinach & Rocket Smoothie')
-streamlit.txt('Hard Boiled Free Range Egg')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard Boiled Free Range Egg')
