af974b4b3c60d7c008203555d17d98a9e0ba54d9
rmareedu2293 <31863929+rmareedu2293@users.noreply.github.com>
2022-06-15 12:24:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 #import snowflake.connector
 streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -10,12 +11,13 @@ myfruit_list=myfruit_list.set_index('Fruit')
 #streamlit.multiselect("Pick some fruits",list(myfruit_list.index),['Avocado','Strawberries'])
 fruits_selected=streamlit.multiselect("Pick some fruits",list(myfruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=myfruit_list.loc[fruits_selected]
-streamlit.dataframe(myfruit_list)
+streamlit.dataframe(fruits_to_show)
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 import snowflake.connector
 my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-#my_cur.execute("SELECT CURRENT_USER(),CURRENT_ACCOUNT(),CURRENT_REGION()")
+#my_cur.execute("select * from fruit_load_list")
+my_cur.execute("SELECT CURRENT_USER(),CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row=my_cur.fetchone()
 streamlit.text("HThefruit load list contains")
 streamlit.text(my_data_row)
